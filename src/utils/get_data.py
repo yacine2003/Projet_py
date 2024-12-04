06a7cecf7ed8cadad432e94be6e467739366c24b
yacine2003 <yacinehousny3@gmail.com>
2024-12-04 14:28:49 +0100

    Ajout de get_data.py

---
+++ src/utils/get_data.py
@@ -0,0 +1,28 @@
+import requests
+
+
+def download_data(url: str, save_path: str) -> None:
+    """
+    Télécharge un fichier depuis une URL et le sauvegarde localement.
+
+    Args:
+        url (str): L'URL du fichier à télécharger.
+        save_path (str): Chemin où sauvegarder le fichier téléchargé.
+    """
+    try:
+        response = requests.get(url)
+
+        with open(save_path, "wb") as file:
+            file.write(response.content)
+
+        print(f"Fichier téléchargé avec succès : {save_path}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Erreur lors du téléchargement : {e}")
+
+
+
+dataset_url = "https://public.opendatasoft.com/explore/dataset/us-cities-demographics/download/?format=csv"
+save_location = "data/raw/dataset.csv"
+
+download_data(dataset_url, save_location)
